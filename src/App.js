c4adc58452da0d5b1db19a1b96f86c89da8bc6cc
Shivam kumar <shivamkumarhost@gmail.com>
2023-09-24 23:39:37 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { Route, Router, Routes } from "react-router-dom";
+import {BrowserRouter as  Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home"
 
 
 import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
 
 
 function App() {
@@ -14,7 +17,7 @@ function App() {
 
        <Routes>
             
-            <Route/>
+             <Route path="/" element={<Home/>}/> 
   
        </Routes>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+
 
 const Header=()=>{
     return(
-        <div>
-           <h1>hii</h1>
-        </div>
+        <nav>
+            <h1>TechStar</h1>
+            <main>
+                <Link to={"/"}>Home</Link>
+                  <Link to={"/contact"}>Contact</Link>
+                    <Link to={"/#about"}>About</Link>
+                      <Link to={"#brands"}>Brands</Link>
+                        <Link to={"/services"}>Services</Link>
+            </main>
+        </nav>
     )
 }
 
